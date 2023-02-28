064c64c56d95c5077de083015d3421bed763d97c
unknown <spicedexdee123@gmail.com>
2023-02-28 19:23:23 +0700

    Add Register

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,13 @@ import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../components/Home.vue'
 import Login from '../components/Login.vue'
 import Lab from '../components/Lab.vue'
+import Register from '../components/Register.vue'
 
 const routes = [
     {path: '/', component: Home},
     {path: '/Login', component: Login},
-    {path: '/lab', component: Lab}
+    {path: '/lab', component: Lab},
+    {path: '/Register', component: Register}
 ]
 
 const  router = createRouter({
