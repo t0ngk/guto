126c5cae1eb0e51c2ab23dc1c7c5c3e1d363574a
C0M3TZZ <39180519+C0M3TZZ@users.noreply.github.com>
2023-03-02 05:19:58 +0700

    🔧 : Change Vue 2 to 3

--- src/components/Appointment.vue
+++ src/components/Appointment.vue
@@ -1,50 +1,42 @@
 <script setup>
-import { Button } from 'flowbite-vue'
-import { Input } from 'flowbite-vue'
+import { Button, Input } from 'flowbite-vue'
+import { ref } from 'vue'
 
-</script>
-<script>
-export default {
-    name: 'Appointment',
-    data() {
-        return {
-            checkRadio: '',
-            PetCare: {
-                first: 'บริการอาบน้ำตัดขนสัตว์เลี้ยง',
-                second: 'บริการสระว่ายน้ำสุนัก',
-                third: 'บริการรับฝากสัตว์เลี้ยง',
-            },
-            PetHealth: {
-                first: 'บริการตรวจสุขภาพสัตว์เลี้ยง',
-            },
-            service: {
-                PetCare: 'PetCare',
-                PetHealth: 'PetHealth',
-            },
-            selection: {},
-            state: 1,
-            dateApptionment: '',
-            timeApptionment: '',
-            AllTime: {
-                morning: {
-                    first: '9.00 - 10.00',
-                    second: '10.00 - 11.00',
-                    third: '11.00 - 12.00',
-                },
-                afternoon: {
-                    first: '13.00 - 14.00',
-                    second: '14.00 - 15.00',
-                    third: '15.00 - 16.00',
-                },
-                evening: {
-                    first: '17.00 - 18.00',
-                    second: '18.00 - 19.00',
-                    third: '19.00 - 20.00',
-                },
-            }
-        }
+let checkRadio = ref('');
+const PetCare = {
+    first: 'บริการอาบน้ำตัดขนสัตว์เลี้ยง',
+    second: 'บริการสระว่ายน้ำสุนัก',
+    third: 'บริการรับฝากสัตว์เลี้ยง',
+}
+const PetHealth = {
+    first: 'บริการตรวจสุขภาพสัตว์เลี้ยง',
+}
+const service = {
+    PetCare: 'PetCare',
+    PetHealth: 'PetHealth',
+}
+let selection = ref({});
+let state = ref(1);
+let dateApptionment = ref('');
+let timeApptionment = ref('');
+const AllTime = {
+    morning: {
+        first: '9.00 - 10.00',
+        second: '10.00 - 11.00',
+        third: '11.00 - 12.00',
+    },
+    afternoon: {
+        first: '13.00 - 14.00',
+        second: '14.00 - 15.00',
+        third: '15.00 - 16.00',
+    },
+    evening: {
+        first: '17.00 - 18.00',
+        second: '18.00 - 19.00',
+        third: '19.00 - 20.00',
     },
 }
+
 </script>
 
 <template>
@@ -63,13 +55,14 @@ export default {
                 <div class="flex items-center pl-4 border border-gray-200 rounded w-1/6">
                     <input id="bordered-radio-1" type="radio" value="PetCare" name="bordered-radio"
                         class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500" v-model="checkRadio">
-                    <label for="bordered-radio-1" class="w-full py-4 ml-2 text-sm font-medium text-gray-900">PetCare</label>
+                    <label for="bordered-radio-1" class="w-full py-4 ml-2 text-sm font-medium text-gray-900">
+                        PetCare</label>
                 </div>
                 <div class="flex items-center pl-4 border border-gray-200 rounded w-1/6">
                     <input id="bordered-radio-1" type="radio" value="PetHealth" name="bordered-radio"
                         class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500" v-model="checkRadio">
-                    <label for="bordered-radio-1"
-                        class="w-full py-4 ml-2 text-sm font-medium text-gray-900">PetHealth</label>
+                    <label for="bordered-radio-1" class="w-full py-4 ml-2 text-sm font-medium text-gray-900">
+                        PetHealth</label>
                 </div>
             </div>
             <h1 class=" text-xl font-semibold">
@@ -82,7 +75,8 @@ export default {
                     class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600">
                 <label for="bordered-checkbox-1" class="w-full py-4 ml-2 text-sm font-medium text-gray-900">{{ n }}</label>
             </div>
-            <div class="flex items-center pl-4 border border-gray-200 rounded my-5" v-if="checkRadio == 'PetHealth'" v-for="n in PetHealth">
+            <div class="flex items-center pl-4 border border-gray-200 rounded my-5" v-if="checkRadio == 'PetHealth'"
+                v-for="n in PetHealth">
                 <input id="bordered-checkbox-1" type="checkbox" value="" name="bordered-checkbox"
                     class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600">
                 <label for="bordered-checkbox-1" class="w-full py-4 ml-2 text-sm font-medium text-gray-900">{{ n }}</label>
@@ -95,31 +89,37 @@ export default {
             <h1 class="text-xl font-semibold mt-5">เวลาที่ต้องการจะนัด</h1>
             <hr class="mb-5">
             <p class=" text-xl font-medium"> รอบเช้า </p>
-            <div class="flex flex-row items-center pl-4 w-1/6 border border-gray-200 rounded mb-3" v-for="time in AllTime.morning">
+            <div class="flex flex-row items-center pl-4 w-1/6 border border-gray-200 rounded mb-3"
+                v-for="time in AllTime.morning">
                 <input id="bordered-checkbox-1" type="radio" value="" name="bordered-checkbox"
-                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 " v-model="timeApptionment">
-                <label for="bordered-checkbox-1"
-                    class="w-full py-4 ml-2 text-sm font-medium text-gray-900">{{ time}}</label>
+                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 "
+                    v-model="timeApptionment">
+                <label for="bordered-checkbox-1" class="w-full py-4 ml-2 text-sm font-medium text-gray-900">{{
+                    time }}</label>
             </div>
 
             <p class=" text-xl font-medium"> รอบเช้า </p>
-            <div class="flex flex-row items-center pl-4 w-1/6 border border-gray-200 rounded mb-3" v-for="time in AllTime.afternoon">
+            <div class="flex flex-row items-center pl-4 w-1/6 border border-gray-200 rounded mb-3"
+                v-for="time in AllTime.afternoon">
                 <input id="bordered-checkbox-1" type="radio" value="" name="bordered-checkbox"
-                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 " v-model="timeApptionment">
-                <label for="bordered-checkbox-1"
-                    class="w-full py-4 ml-2 text-sm font-medium text-gray-900">{{ time}}</label>
+                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 "
+                    v-model="timeApptionment">
+                <label for="bordered-checkbox-1" class="w-full py-4 ml-2 text-sm font-medium text-gray-900">{{
+                    time }}</label>
             </div>
 
             <p class=" text-xl font-medium"> รอบเช้า </p>
-            <div class="flex flex-row items-center pl-4 w-1/6 border border-gray-200 rounded mb-3" v-for="time in AllTime.evening">
+            <div class="flex flex-row items-center pl-4 w-1/6 border border-gray-200 rounded mb-3"
+                v-for="time in AllTime.evening">
                 <input id="bordered-checkbox-1" type="radio" value="" name="bordered-checkbox"
-                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 " v-model="timeApptionment">
-                <label for="bordered-checkbox-1"
-                    class="w-full py-4 ml-2 text-sm font-medium text-gray-900">{{ time}}</label>
+                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 "
+                    v-model="timeApptionment">
+                <label for="bordered-checkbox-1" class="w-full py-4 ml-2 text-sm font-medium text-gray-900">{{
+                    time }}</label>
             </div>
         </div>
         <div class="container mx-auto my-16" v-if="state == 3">
-            
+
         </div>
         <Button class="bg-primary" @click="state++">Next</Button>
     </div>
