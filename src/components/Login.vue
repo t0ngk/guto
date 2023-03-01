9727f7b5eefa18dc5c6861c6313958fe8cbfdb7c
C0M3TZZ <39180519+C0M3TZZ@users.noreply.github.com>
2023-03-02 02:37:59 +0700

    💄 : Redesign Login Page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,10 +1,34 @@
 <script setup>
-    import { TheCard } from 'flowbite-vue'
-    import { Input } from 'flowbite-vue'
-    import { Button } from 'flowbite-vue'
+import { TheCard } from 'flowbite-vue'
+import { Input } from 'flowbite-vue'
+import { Button } from 'flowbite-vue'
 </script>
 <template>
-    <div class="container mx-auto flex my-64 gap-40 justify-center items-center">
+    <div class="w-screen h-screen flex">
+        <div class="w-1/2 bg-primary-400 md:flex flex-col justify-center items-center hidden">
+            <img src="src/assets/Dog.svg" alt="" srcset="" class="w-1/4">
+            <h1 class="text-5xl text-white">Guto</h1>
+        </div>
+        <div class="w-full md:w-1/2 flex justify-center items-center p-4">
+            <div class="shadow p-4">
+                <div class="flex flex-col gap-4">
+                    <h1 class="text-xl text-center">Login</h1>
+                    <div class="flex flex-col md:flex-row gap-4">
+                        <div class="w-full">
+                            <Input placeholder="Enter your Username" label="Username" />
+                        </div>
+                        <div class="w-full">
+                            <Input placeholder="Enter your Password" label="Password" />
+                        </div>
+                    </div>
+                    <div class="flex justify-end">
+                        <Button class="bg-primary hover:bg-primary-400 transition-all">Login</Button>
+                    </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- <div class="container mx-auto flex my-64 gap-40 justify-center items-center">
         <div class="">
             <img src="src/assets/Dog.svg" class=" w-60">
         </div>
@@ -20,9 +44,9 @@
                     </div>
                     <div class="flex flex-col gap-2">
                         <Button color="yellow">Login</Button>
+                                </div>
+                            </div>
+                        </TheCard>
                     </div>
-                </div>
-            </TheCard>
-        </div>
-    </div>
+                </div> -->
 </template>
