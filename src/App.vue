dcf01feb82f5470a49b0509d6a8dc09c0785b15f
C0M3TZZ <39180519+C0M3TZZ@users.noreply.github.com>
2023-03-02 01:06:40 +0700

    ✨ : Hide Navbar in auth url.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <script setup>
 import Navbar from './components/Navbar.vue'
 import Footer from './components/footer.vue'
+import { ref, watch } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+watch(route, () => {
+  pathname.value = route.path;
+});
+
+const pathname = ref();
+const noNeedNavBar = ['/Login', '/Register'];
+
 </script>
 
 <template>
-  <Navbar />
+  <Navbar v-if="!(noNeedNavBar.includes(pathname))" />
   <router-view></router-view>
-  <Footer />
+  <Footer v-if="!(noNeedNavBar.includes(pathname))" />
 </template>
