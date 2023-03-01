43092f61f2c80ce2bd5cfa7b4ce8f87025a2be15
unknown <spicedexdee123@gmail.com>
2023-03-02 00:24:00 +0700

    :lipstick: : Add or update the UI and style files.

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,16 @@ import Home from '../components/Home.vue'
 import Login from '../components/Login.vue'
 import Lab from '../components/Lab.vue'
 import Register from '../components/Register.vue'
+import About from '../components/About.vue'
+import Appointment from '../components/Appointment.vue'
 
 const routes = [
     {path: '/', component: Home},
     {path: '/Login', component: Login},
     {path: '/lab', component: Lab},
-    {path: '/Register', component: Register}
+    {path: '/Register', component: Register},
+    {path: '/About', component: About},
+    {path: '/Appointment', component: Appointment},
 ]
 
 const  router = createRouter({
