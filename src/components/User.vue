59fb87bd0cb04d62a669b12b3d957de83f4c0482
C0M3TZZ <39180519+C0M3TZZ@users.noreply.github.com>
2023-03-02 23:07:09 +0700

    ✨ : Bind User to User Component

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { Button, Input } from 'flowbite-vue'
 import { Icon } from '@iconify/vue';
-import { ref } from 'vue'
+import { onMounted, reactive, ref } from 'vue'
 const Sidebar = [{
   title: 'User',
   icon: 'material-symbols:person',
@@ -75,27 +75,32 @@ const menus = {
     ]
   }]
 }
+
+const users = JSON.parse(localStorage.getItem('users')) || [];
+const userId = JSON.parse(localStorage.getItem('user'));
+let user = reactive(users.find(user => user.id === userId));
+
 </script>
 
 <template>
   <div class="flex justify-center items-center w-full p-4">
-    <div class="flex shadow-2xl p-4 gap-4 rounded-lg w-3/4">
-      <aside class="z-40 w-1/4 h-full transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-        <div class="px-3 rounded-lg py-4 overflow-y-auto bg-primary-100 dark:bg-gray-800">
-          <ul class="space-y-2">
-            <li v-for="menu in Sidebar">
-              <a @click="active = menu.value"
-                :class="[`flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-primary hover:text-white transition-all dark:hover:bg-gray-700`, active == menu.value ? `bg-primary text-white` : ``]">
-                <span>
-                  <Icon :icon="menu.icon" class="w-6 h-6" />
-                </span>
-                <span class="ml-3">{{ menu.title }}</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </aside>
-      <div class="flex w-3/4 flex-wrap">
+    <div class="flex shadow-2xl h-[35rem] p-4 gap-4 rounded-lg w-3/4 border">
+        <aside class="z-40 flex flex-col w-1/4 transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+          <div class="px-3 rounded-lg h-full py-4 overflow-y-auto bg-primary-100 dark:bg-gray-800">
+            <ul class="space-y-2">
+              <li v-for="menu in Sidebar">
+                <a @click="active = menu.value"
+                  :class="[`flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-primary hover:text-white transition-all dark:hover:bg-gray-700`, active == menu.value ? `bg-primary text-white` : ``]">
+                  <span>
+                    <Icon :icon="menu.icon" class="w-6 h-6" />
+                  </span>
+                  <span class="ml-3">{{ menu.title }}</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </aside>
+      <div class="flex w-3/4 flex-wrap h-full overflow-y-auto">
         <div v-if="active == 'user'" class="w-full p-4">
           <div class="flex justify-between gap-4 w-full">
             <H1 class="text-xl">User</H1>
@@ -105,14 +110,16 @@ const menus = {
             </div>
           </div>
           <div class="my-4">
-            <Input :disabled="true" placeholder="enter your First name" value="" label="ชื่อ" />
-            <Input :disabled="true" placeholder="enter your Last name" value="" label="นามสกุล" />
-            <Input :disabled="true" placeholder="enter your Phone number" value="" label="เบอร์โทรศัพท์" />
+            
+            <Input :disabled="true" placeholder="enter your First name" :value="user.first_name" label="ชื่อ" />
+            <Input :disabled="true" placeholder="enter your Last name" :value="user.last_name" label="นามสกุล" />
+            <Input :disabled="true" placeholder="enter your Phone number" :value="user.phone_number" label="เบอร์โทรศัพท์" />
             <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ที่อยู่</label>
             <textarea id="message" rows="2" :disabled="true"
               class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Write your thoughts here..."></textarea>
-            <Input :disabled="true" placeholder="enter your Email" value="" label="อีเมล" />
+              placeholder="Write your thoughts here..."
+              :value="user.address"></textarea>
+            <Input :disabled="true" placeholder="enter your Email" :value="user.email" label="อีเมล" />
           </div>
           <div class="flex justify-end">
             <Button>Log out</Button>
@@ -120,7 +127,7 @@ const menus = {
         </div>
         <div v-else class="flex gap-4 w-full flex-wrap">
           <div v-for="item in menus[active]" class="w-1/4">
-            <div class=" shadow-2xl rounded-lg p-4 flex flex-col gap-4 w-full">
+            <div class=" shadow-2xl border rounded-lg p-4 flex flex-col gap-4 w-full">
               <h1 class="text-xl">{{ item.title }}</h1>
               <p v-for="desc in item.desc">
                 {{ desc.title }} : {{ desc.value }}
