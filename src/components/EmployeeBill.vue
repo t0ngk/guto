8833e7e277677552d024e12f4c1dce69c1f0d584
Phe-nix <88503925+Phe-nix@users.noreply.github.com>
2023-03-03 01:29:47 +0700

    :lipstick: : Component for table

---
+++ src/components/EmployeeBill.vue
@@ -0,0 +1,113 @@
+<script setup>
+import { Input } from 'flowbite-vue'
+import { Modal } from 'flowbite-vue'
+import { ref } from 'vue'
+
+const isShowModal = ref(false)
+function closeModal() {
+  isShowModal.value = false
+}
+function showModal() {
+  isShowModal.value = true
+}
+</script>
+<template>
+  <div class="container mx-auto my-4">
+
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <div class="pb-4 bg-white dark:bg-gray-900">
+        <label for="table-search" class="sr-only">Search</label>
+        <div class="relative mt-1">
+          <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor"
+              viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                clip-rule="evenodd"></path>
+            </svg>
+          </div>
+          <div class="flex justify-between">
+            <input type="text" id="table-search"
+              class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search for items">
+            <button @click="showModal" type="button"
+              class=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Add Appointment
+            </button>
+          </div>
+        </div>
+      </div>
+      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+          <tr>
+            <th scope="col" class="px-6 py-3">
+              Id
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Details
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Date
+            </th>
+            <th scope="col" class="px-6 py-3">
+              State
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Edit
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+              1
+            </th>
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowr">
+              ค่าชำระยา
+            </th>
+            <td class="px-6 py-4">
+              2021-01-01
+            </td>
+            <td class="px-6 py-4">
+              Not Paid
+            </td>
+            <td class="px-6 py-4">
+              <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  <Modal :size="size" v-if="isShowModal" @close="closeModal">
+    <template #header>
+      <div class="flex items-center text-lg">
+        Add Appointment
+      </div>
+    </template>
+    <template #body>
+      <div class="flex flex-col">
+        <Input placeholder="Details Bill" label="Details" />
+        <Input type="date" placeholder="Date of bill" label="Date" />
+        <label for="petType" class="text-sm font-medium text-gray-700 dark:text-gray-200">State</label>
+        <select id="petType"
+          class="block p-2 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          <option value="dog">ยังไม่ชำระเงิน</option>
+          <option value="cat">ชำระเงินแล้ว</option>
+        </select>
+      </div>
+    </template>
+    <template #footer>
+      <div class="flex justify-between">
+        <button @click="closeModal" type="button"
+          class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+          Decline
+        </button>
+        <button @click="closeModal" type="button"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          I accept
+        </button>
+      </div>
+    </template>
+  </Modal>
+</template>
---
+++ src/components/PermissionTable.vue
@@ -0,0 +1,75 @@
+<script setup>
+</script>
+
+
+
+<template>
+  <div class="container mx-auto my-4">
+
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <div class="pb-4 bg-white dark:bg-gray-900">
+        <label for="table-search" class="sr-only">Search</label>
+        <div class="relative mt-1">
+          <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor"
+              viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                clip-rule="evenodd"></path>
+            </svg>
+          </div>
+          <div class="flex justify-between">
+            <input type="text" id="table-search"
+              class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search for items">
+          </div>
+        </div>
+      </div>
+      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" class="px-6 py-3">
+              Id
+            </th>
+            <th scope="col" class="px-6 py-3">
+              User name
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Email
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Permission
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Phone Nummber
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Edit
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+              1
+            </th>
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowr">
+              ธวิชญ์ ศรีสุข
+            </th>
+            <td class="px-6 py-4">
+              ธวิชญ์.ศรีสุข@gmail.com
+            </td>
+            <td class="px-6 py-4">
+              admin
+            </td>
+            <td class="px-6 py-4">
+              081-234-5678
+            </td>
+            <td class="px-6 py-4">
+              <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+            </td>
+          </tr>
+        </tbody>
+    </table>
+  </div>
+</div></template>
