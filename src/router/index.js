59ae07333324cb21e01bc7719b47408e168c1399
Phe-nix <88503925+Phe-nix@users.noreply.github.com>
2023-03-02 22:22:55 +0700

    :wrench: : Update router

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Home from "../components/Home.vue";
 import Login from "../components/Login.vue";
 import Lab from "../components/Lab.vue";
 import Register from "../components/Register.vue";
-import About from "../components/About.vue";
+import User from "../components/User.vue";
 import Appointment from "../components/Appointment.vue";
 
 const routes = [
@@ -12,7 +12,7 @@ const routes = [
   { path: "/login", component: Login },
   { path: "/lab", component: Lab },
   { path: "/register", component: Register },
-  { path: "/about", component: About },
+  { path: "/user", component: User },
   { path: "/appointment", component: Appointment },
 ];
 
