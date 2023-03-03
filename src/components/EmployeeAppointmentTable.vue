0edc69e02a47d8c1a0b6b3c6f8e37c44447aee36
unknown <spicedexdee123@gmail.com>
2023-03-03 13:30:53 +0700

    :lipstick: : Update Button Delete

--- src/components/EmployeeAppointmentTable.vue
+++ src/components/EmployeeAppointmentTable.vue
@@ -198,8 +198,8 @@ const deleteAppointment = () => {
         <Input type="date" placeholder="enter your Desciption" label="Date" name="date" />
         <div class="flex justify-between">
           <button @click="closeModal" type="button"
-            class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
-            Cancel
+            class="text-white bg-red-500 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+            Delete
           </button>
           <button type="submit"
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
