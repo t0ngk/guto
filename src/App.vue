78b10aa988740b20c4f96c84c9f89ae4f9286c87
unknown <spicedexdee123@gmail.com>
2023-03-17 03:28:26 +0700

    :lipstick: : Add Ui main

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
+<script setup>
+
+import { ref, watch } from 'vue';
+import { useRoute } from 'vue-router';
+import Navber from './views/Navbar.vue';
+
+const route = useRoute();
+watch(route, () => {
+  pathname.value = route.path.toLowerCase();
+});
+
+const pathname = ref();
+const noNeedNavBar = ['/login', '/register'];
+</script>
 <template>
-  <n-space>
-    <n-button>Default</n-button>
-    <n-button type="tertiary">
-      Tertiary
-    </n-button>
-    <n-button type="primary">
-      Primary
-    </n-button>
-    <n-button type="info">
-      Info
-    </n-button>
-    <n-button type="success">
-      Success
-    </n-button>
-    <n-button type="warning">
-      Warning
-    </n-button>
-    <n-button type="error">
-      Error
-    </n-button>
-  </n-space>
+  <Navber/>
+  <router-view></router-view>
 </template>
