511b3473f91a11b324a2c7a0c468ab64ec6ce437
unknown <spicedexdee123@gmail.com>
2023-03-18 02:20:05 +0700

    :hammer: : Config Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
 
 const routes = [
     { path: "/", component: Home },
+    { path: "/login", component: Login },
+    { path: "/register", component: Register },
   ];
   
   const router = createRouter({
