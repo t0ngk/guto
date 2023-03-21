6b7a0b89a54bc141fc3df3e5c9933157edace140
unknown <spicedexdee123@gmail.com>
2023-03-21 21:09:24 +0700

    :lipstick: : Add component for Login

---
+++ src/components/FormLogin.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="w-full md:w-1/2 flex justify-center items-center p-4">
+        <n-form ref="formRef" :label-width="80" :model="formValue" :rules="rules" :size="'medium'">
+            <n-form-item label="Email" path="formValue.email">
+                <n-input round v-model:value="formValue.email" placeholder="Input Name" />
+            </n-form-item>
+            <n-form-item label="Password" path="formValue.password">
+                <n-input round v-model:value="formValue.password" placeholder="Input Age" />
+            </n-form-item>
+            <n-form-item>
+                <n-button round @click="handleValidateClick">
+                    Login
+                </n-button>
+                <n-a href="/Register">สมัครสมาชิก</n-a>
+            </n-form-item>
+        </n-form>
+    </div>
+</template>
+
+<script setup>
+import { useMessage } from "naive-ui";
+const message = useMessage();
+const formValue = ref({
+    email: '',
+    password: ''
+})
+const formRef = ref(null)
+
+const rules = {
+    formValue: {
+        email: {
+            required: true,
+            message: 'กรุณากรอกอีเมลของคุณ',
+            trigger: ['blur', 'input']
+        },
+        password: {
+            required: true,
+            message: 'กรุณากรอกรหัสผ่านของคุณ',
+            trigger: 'blur'
+        }
+    }
+}
+
+const handleValidateClick = (e) => {
+    e.preventDefault();
+    formRef.value?.validate((errors) => {
+        if (!errors) {
+            message.success("Valid");
+        } else {
+            console.log(errors);
+            message.error("Invalid");
+        }
+    });
+}
+</script>
