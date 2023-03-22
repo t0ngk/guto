45f11015868802a07ec68c8bdd065d6fabcd88d5
unknown <spicedexdee123@gmail.com>
2023-03-23 01:00:59 +0700

    :lipstick: : Add appointment Ui

---
+++ src/views/Appointment.vue
@@ -0,0 +1,24 @@
+<script setup>
+const currentRef = ref(1);
+</script>
+
+<template>
+    <div class="container mx-auto">
+        <div class="my-10 border ">
+            <n-h1 class="text-center">Appointment</n-h1>
+            <div class="my-16 mx-auto">
+                <n-steps v-model:current="currentRef">
+                    <n-step title="ข้อมูลสัตว์เลี้ยง" />
+                    <n-step title="ข้อมูลการนัดหมาย" />
+                    <n-step title="เลือกบริการ" />
+                    <n-step title="ยื่นยันการนัด" />
+                    <n-step title="รอการติดต่อกลับ" />
+                </n-steps>
+            </div>
+            <n-divider />
+            <div name="step1" class="">
+                
+            </div>
+        </div>
+    </div>
+</template>
