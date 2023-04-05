4a34c5185d24cc08d8b13811ae02ea585f3d4056
unknown <spicedexdee123@gmail.com>
2023-04-05 18:18:09 +0700

    :wrench: : config main theme

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,22 @@ import Navber from './views/Navbar.vue';
 
 const route = useRoute();
 
+const themeOverrides = {
+  common: {
+    "primaryColor": "#de7d24FF",
+    "primaryColorHover": "#c45d1bFF",
+    "primaryColorPressed": "#e69c39FF",
+    "primaryColorSuppl": "#de7d24FF",
+    "baseColor": "#FFF",
+    "cardColor": "#F8F1F1FF",
+    "dividerColor": "rgba(200, 200, 200, 1)",
+    "bodyColor": "#F8F1F1FF"
+  },
+  Button: {
+    "textColor": "rgba(0, 0, 0, 0.65)",
+    "color": "#000"
+  }
+}
 
 watch(route, () => {
   pathname.value = route.path.toLowerCase();
@@ -15,8 +31,10 @@ const pathname = ref();
 const noNeedNavBar = ['/login', '/register'];
 </script>
 <template>
-  <Navber v-if="!(noNeedNavBar.includes(pathname))" />
-  <n-message-provider>
-    <router-view></router-view>
-  </n-message-provider>
+  <n-config-provider :theme-overrides="themeOverrides">
+    <Navber v-if="!(noNeedNavBar.includes(pathname))" />
+    <n-message-provider>
+      <router-view></router-view>
+    </n-message-provider>
+  </n-config-provider>
 </template>
