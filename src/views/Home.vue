bf2551e40564654d2c3b81f9bc288a9085930186
unknown <spicedexdee123@gmail.com>
2023-04-12 18:55:28 +0700

    :lipstick: : Update landing Ui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,8 +17,8 @@ let reveal = () => {
         }
     }
 }
-
 window.addEventListener("scroll", reveal);
+
 </script>
 <template>
     <section class="my-32">
@@ -68,6 +68,44 @@ window.addEventListener("scroll", reveal);
             </div>
         </div>
     </section>
+
+    <section class="my-64 reveal">
+        <n-h1 class="text-center font-semibold"> ที่ตั้งคลินิก </n-h1>
+        <div class="flex justify-center">
+            <div class="">
+                <iframe class=""
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3875.790312141103!2d100.77900356483022!3d13.73114134036085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x311d66308ce98ffd%3A0xcb43a76f038c38ca!2z4LiE4LiT4Liw4LmA4LiX4LiE4LmC4LiZ4LmC4Lil4Lii4Li14Liq4Liy4Lij4Liq4LiZ4LmA4LiX4LioIOC4quC4luC4suC4muC4seC4meC5gOC4l-C4hOC5guC4meC5guC4peC4ouC4teC4nuC4o-C4sOC4iOC4reC4oeC5gOC4geC4peC5ieC4suC5gOC4iOC5ieC4suC4hOC4uOC4k-C4l-C4q-C4suC4o-C4peC4suC4lOC4geC4o-C4sOC4muC4seC4hyAoSVRLTUlUTCk!5e0!3m2!1sth!2sth!4v1681292986331!5m2!1sth!2sth"
+                    width="800" height="450" style="border:1;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    </section>
+
+    <footer class="bg-primary-400">
+        <div class="py-5 flex flex-col">
+            <div class="flex flex-col">
+                <n-p class="text-center font-semibold text-xl">ช่องทางการติดต่อ</n-p>
+                <div class="flex flex-col gap-y-3">
+                    <div class="flex gap-5 justify-center">
+                        <a href="https://www.facebook.com/ITLadkrabang">
+                            <Icon icon="mdi:facebook" class="text-4xl" />
+                        </a>
+                        <a href="https://www.instagram.com/kmitlofficial/">
+                            <Icon icon="mdi:instagram" class="text-4xl" />
+                        </a>
+                    </div>
+                    <div class="flex flex-col items-center gap-y-0">
+                        <n-p>อีเมล : 64070047@kmitl.ac.th</n-p>
+                        <n-p>เบอร์โทร : 064-070-047</n-p>
+                    </div>
+                </div>
+                <div class="text-center text-lg px-10">
+                    <n-p>คณะเทคโนโลยีสารสนเทศ สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง เลขที่ 1 ซอยฉลองกรุง 1
+                        แขวงลาดกระบัง เขตลาดกระบัง กรุงเทพฯ 10520 ติดต่อ +66 (0) 2723 4900 หรือ +66 (0) 2723 4910</n-p>
+                </div>
+            </div>
+        </div>
+    </footer>
 </template>
 
 <style scoped>
